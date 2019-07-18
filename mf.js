593a4f5de0eb96aad6dde5a9f6e5b44bf41edd26
David Volm <david@volminator.com>
2019-07-18 08:57:31 -0500

    boilerplate

---
+++ mf.js
@@ -0,0 +1,7 @@
+/* Midi Fighter Player
+ * HTML5 Midi Fighter interface
+ * (c) 2019 David (daXXog) Volm ><> + + + <><
+ * Released under Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0.html  
+ */ 
+
